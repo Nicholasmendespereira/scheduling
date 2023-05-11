1a996b989522547086aa23e3f527f4bcf8b96069
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-05-11 18:08:31 -0300

    up

--- scheduling/src/Nav/index.jsx
+++ scheduling/src/Nav/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import { Dialog, Disclosure, Popover, Transition, Menu } from '@headlessui/react'
 import {
     ArrowPathIcon,
     Bars3Icon,
@@ -11,6 +11,7 @@ import {
     XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import { BellIcon } from "@heroicons/react/24/outline";
 
 
 const products = [
@@ -24,6 +25,17 @@ const callsToAction = [
     { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
     { name: 'Contact sales', href: '#', icon: PhoneIcon },
 ]
+const userNavigation = [
+    { name: "Your Profile", href: "#" },
+    { name: "Settings", href: "#" },
+    { name: "Sign out", href: "#" },
+];
+const user = {
+    name: "Tom Cook",
+    email: "tom@example.com",
+    imageUrl:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
@@ -59,7 +71,7 @@ function Nav() {
                     </div>
                     <Popover.Group className="hidden lg:flex lg:gap-x-12">
                         <Popover className="relative">
-                            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+                            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-slate-300">
                                 Product
                                 <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
                             </Popover.Button>
@@ -119,7 +131,55 @@ function Nav() {
                             Company
                         </a>
                     </Popover.Group>
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    {/* Profile dropdown */}
+                    <div className="hidden flex justify-items-end lg:flex lg:flex-1 lg:justify-end">
+                        <button
+                            type="button"
+                            className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        >
+                            <span className="sr-only">View notifications</span>
+                            <BellIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+
+                        <Menu as="div" className="mx-3">
+                            <div>
+                                <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                    <span className="sr-only">Open user menu</span>
+                                    <img
+                                        className="h-8 w-8 rounded-full"
+                                        src={user.imageUrl}
+                                        alt=""
+                                    />
+                                </Menu.Button>
+                            </div>
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                            >
+                                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    {userNavigation.map((item) => (
+                                        <Menu.Item key={item.name}>
+                                            {({ active }) => (
+                                                <a
+                                                    href={item.href}
+                                                    className={classNames(
+                                                        active ? "bg-gray-100" : "",
+                                                        "block px-4 py-2 text-sm text-gray-700"
+                                                    )}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            )}
+                                        </Menu.Item>
+                                    ))}
+                                </Menu.Items>
+                            </Transition>
+                        </Menu>
                         <a href="#" className="text-sm font-semibold leading-6 text-slate-300">
                             Log in <span aria-hidden="true">&rarr;</span>
                         </a>

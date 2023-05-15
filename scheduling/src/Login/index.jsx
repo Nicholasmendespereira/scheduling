10f70ad93dcee489e2502ca00615c6ee288dd63d
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-05-14 22:18:18 -0300

    funtion-verify-login
    Neste commit foi realizado a criação da função de verificação de usuário;

--- scheduling/src/Login/index.jsx
+++ scheduling/src/Login/index.jsx
@@ -1,4 +1,31 @@
+import api from "../../api/index";
+import { useState, useEffect } from "react";
+
 function Login() {
+    const [Userdata, setUserdata] = useState({});
+
+  const HandleLogin = async () => {
+    try {
+      const resp = await api({
+        method: "POST",
+        url: "/login",
+        data: {
+          name: String(Userdata?.name),
+        //   senha: Userdata?.senha,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+        json: true,
+      });
+      alert("Login feito com sucesso!");
+      console.log({ Userdata });
+    } catch (e) {
+        alert("Error: Usuário não cadastrado!")
+      console.error(e);
+    }
+  };
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -14,7 +41,7 @@ function Login() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          {/* <form className="space-y-6" action="#" method="POST"> */}
             <div>
               <label
                 htmlFor="email"
@@ -26,7 +53,8 @@ function Login() {
                 <input
                   id="email"
                   name="email"
-                  type="email"
+                  type="text"
+                  onChange={(e) => {setUserdata({...Userdata,  name: e.target.value})}}
                   autoComplete="email"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -57,21 +85,22 @@ function Login() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  onChange={(e) => {setUserdata({...Userdata,  senha: e.target.value})}}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-
+                {console.log({Userdata})}
             <div>
               <button
-                type="submit"
+                onClick={HandleLogin}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
             </div>
-          </form>
+          {/* </form> */}
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{" "}

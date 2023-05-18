3ca438cddb0c5920510b2ba4ab01f5d02d2d6f2c
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-05-17 21:25:33 -0300

    up

--- scheduling/src/Login/index.jsx
+++ scheduling/src/Login/index.jsx
@@ -21,7 +21,7 @@ function Login() {
         json: true,
       });
       alert("Login feito com sucesso!");
-      toggleLogin()
+      toggleLogin();
       console.log({ Userdata });
     } catch (e) {
       alert("Error: Usuário não cadastrado!");
@@ -30,11 +30,10 @@ function Login() {
   };
 
   return (
-
     <>
-      {Login === false ?
+      {Login === false ? (
         <>
-          < div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8" >
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               <img
                 className="mx-auto h-10 w-auto"
@@ -42,7 +41,7 @@ function Login() {
                 alt="Your Company"
               />
               <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
+                Faça login em sua conta
               </h2>
             </div>
 
@@ -53,8 +52,7 @@ function Login() {
                   htmlFor="email"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Email address {Login}
-
+                  Nome:
                 </label>
                 <div className="mt-2">
                   <input
@@ -66,12 +64,12 @@ function Login() {
                     }}
                     autoComplete="email"
                     required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="mb-10 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
-              <div>
+              {/* <div>
                 <div className="flex items-center justify-between">
                   <label
                     htmlFor="password"
@@ -101,7 +99,7 @@ function Login() {
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
-              </div>
+              </div> */}
               {console.log({ Userdata })}
               <div>
                 <button
@@ -114,19 +112,20 @@ function Login() {
               {/* </form> */}
 
               <p className="mt-10 text-center text-sm text-gray-500">
-                Not a member?{" "}
+                Não está cadastrada?{" "}
                 <a
                   href="#"
                   className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                 >
-                  Start a 14 day free trial
+                  Click aqui!
                 </a>
               </p>
             </div>
-          </div >
+          </div>
         </>
-        : <Navigate to="/home" />
-      }
+      ) : (
+        <Navigate to="/home" />
+      )}
     </>
   );
 }

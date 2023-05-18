1aa7cb4ca6cde1c340a8d7884e76a5aca708070d
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-05-17 22:12:05 -0300

    up

--- scheduling/src/Home/index.jsx
+++ scheduling/src/Home/index.jsx
@@ -15,7 +15,7 @@ function Home() {
     <>
       <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
         <img
-          src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+          src="https://images.unsplash.com/photo-1503951914875-452162b0f3f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
         />

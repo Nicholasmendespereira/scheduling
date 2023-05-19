8da4ea0aa3a834fe23b046d57d9940e81a9f12b4
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-05-19 15:26:12 -0300

    up

--- scheduling/src/Profile/index.jsx
+++ scheduling/src/Profile/index.jsx
@@ -1,7 +1,7 @@
 const people = [
     {
         name: 'Leslie Alexander',
-        role: 'Co-Founder / CEO',
+        role: 'Cliente',
         imageUrl:
             'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
@@ -14,23 +14,21 @@ function Profile() {
             <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                 <div className="max-w-2xl">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Este é o seu perfil</h2>
-                <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+
                     {people.map((person) => (
-                        <li key={person.name}>
-                            <div className="flex items-center gap-x-6">
-                                <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
-                                <div>
-                                    <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
-                                    <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
-                                </div>
+                        <div className="flex items-center gap-x-6 mt-10" key={person.name}>
+                            <img className="h-20 w-20 rounded-full" src={person.imageUrl} alt={person.name} />
+                            <div>
+                                <h3 className="text-2xl font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
+                                <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
                             </div>
-                        </li>
+                        </div>
                     ))}
-                </ul>
+
                 </div>
-                    <p className="mt-6 text-lg leading-8 text-gray-600">
-                       Ultima visita:
-                    </p>
+                <p className="mt-2 text-lg leading-8 text-gray-600">
+                    Ultima visita:
+                </p>
             </div>
         </div>
     );

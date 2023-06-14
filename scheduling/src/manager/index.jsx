6437c6766f164943de45f65e39aca7fdbc3c6f4d
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-06-13 22:32:45 -0300

    up

--- scheduling/src/manager/index.jsx
+++ scheduling/src/manager/index.jsx
@@ -14,21 +14,21 @@ import {
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-// import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-// import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { CalendarDaysIcon } from "@heroicons/react/24/outline";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
-import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import { ExclamationTriangleIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+
 
 function Manager() {
   const [formData, setFormData] = useState({});
   const [users, setUsers] = useState();
   const [open, setOpen] = useState(false);
   const [openDelete, setOpenDelete] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
   const [usersDelete, setUsersDelete] = useState();
   const cancelDeleteButtonRef = useRef(null);
+  const cancelEditButtonRef = useRef(null);
   const cancelButtonRef = useRef(null);
   const Items = [
     {
@@ -127,24 +127,17 @@ function Manager() {
       console.error(e);
     }
   };
-  const user = {
-    name: "Tom Cook",
-    email: "tom@example.com",
-    imageUrl:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  };
-  const navigation = [
-    { name: "Dashboard", href: "#", current: true },
-    { name: "Team", href: "#", current: false },
-    { name: "Projects", href: "#", current: false },
-    { name: "Calendar", href: "#", current: false },
-    { name: "Reports", href: "#", current: false },
-  ];
-  const userNavigation = [
-    { name: "Your Profile", href: "#" },
-    { name: "Settings", href: "#" },
-    { name: "Sign out", href: "#" },
-  ];
+  const handleEditScheduling = async () => {
+    try {
+      const resp = awiat api({
+        method: "PATCH",
+        url: "/update-user",
+        data: {
+          
+        }
+      })
+    }
+  }
   const people = [
     {
       imageUrl:
@@ -210,7 +203,7 @@ function Manager() {
                         </p>
                         <p className="mt-1 text-xs leading-5 text-gray-500 flex items-center	">
                           {moment(person.created_at).format("DD/MM/YYYY")}
-                          <div className="rounded-full bg-red-200 h-6 w-6 ml-1.5 hover:text-white-300 active:bg-red-700 cursor-pointer">
+                          <div className=" flex rounded-full bg-red-200 h-6 w-6 ml-1.5 hover:text-white-300 active:bg-red-700 cursor-pointer">                            
                             <TrashIcon
                               className="h-4 w-4 text-red-600 m-1"
                               onClick={() => {
@@ -219,6 +212,15 @@ function Manager() {
                               }}
                             />
                           </div>
+                           <div className="cursor-pointer">
+                           <PencilSquareIcon
+                              className="h-4 w-4 text-yellow-600 m-1"
+                              onClick={() => {
+                                setOpenDelete(true);
+                                setUsersDelete(person?.id);
+                              }}
+                            />
+                           </div>
                         </p>
                       </div>
                       <Transition.Root show={open} as={Fragment}>
@@ -513,6 +515,85 @@ function Manager() {
                           <div className="sm:flex sm:items-start">
                             <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                               <ExclamationTriangleIcon
+                                className="h-6 w-6 text-red-600"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                              <Dialog.Title
+                                as="h3"
+                                className="text-base font-semibold leading-6 text-gray-900"
+                              >
+                                Deseja excluir esse Agendamento?
+                              </Dialog.Title>
+                              <div className="mt-2">
+                                <p className="text-sm text-gray-500">
+                                  Esse procedimento não poderá ser desfeito!{" "}
+                                  <br />
+                                  ao prosseguir você concorda com o termo acima.
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                          <button
+                            type="button"
+                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                            onClick={() => DeleteSvheduling()}
+                          >
+                            Excluir
+                          </button>
+                          <button
+                            type="button"
+                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                            onClick={() => setOpenDelete(false)}
+                            ref={cancelDeleteButtonRef}
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </Dialog.Panel>
+                    </Transition.Child>
+                  </div>
+                </div>
+              </Dialog>
+            </Transition.Root>
+            <Transition.Root show={openEdit} as={Fragment}>
+              <Dialog
+                as="div"
+                className="relative z-10"
+                initialFocus={cancelEditButtonRef}
+                onClose={setOpenEdit}
+              >
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                  <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                      enterTo="opacity-100 translate-y-0 sm:scale-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                      leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    >
+                      <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                          <div className="sm:flex sm:items-start">
+                            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                              <PencilSquareIcon 
                                 className="h-6 w-6 text-red-600"
                                 aria-hidden="true"
                               />
